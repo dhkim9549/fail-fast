cf423fdc4a438bcad3d3a76dea529c703656906f
root <dhkim95@hotmail.com>
2025-09-23 08:54:42 +0000

    lint

--- app/hf-api/rent-loan-multi-info/get-data.tsx
+++ app/hf-api/rent-loan-multi-info/get-data.tsx
@@ -4,20 +4,20 @@ export async function getLoanRateData(loanYm) {
 
   console.log("getLoanRateData() start...");
 
-  let apiStr = ""
+  const apiStr = ""
     + "?serviceKey=PW2VvwTvkcs%2FWMVLduXzeRL0BPjOYH%2B0wMnsQiyy5UgcrukEjAurATJUNkeA7T%2Bj47s3GAmLzHduip%2BfbxESlQ%3D%3D"
     + "&pageNo=1"
     + "&numOfRows=30"
     + "&dataType=JSON"
     ;
 
-  let res = await fetch("https://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list"
+  const res = await fetch("https://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list"
     + apiStr
     + "&loanYm=" + loanYm,
     { next: { revalidate: 600 } }
   );
 
-  let rentJson = await res.json();
+  const rentJson = await res.json();
 
   console.log("getLoanRateData() end...");
 
