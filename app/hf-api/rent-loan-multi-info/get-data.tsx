f25e85dc85fce99c3641fca100ffaa7e30a16884
root <dhkim95@hotmail.com>
2025-10-24 05:51:40 +0000

    update key, prettier

--- app/hf-api/rent-loan-multi-info/get-data.tsx
+++ app/hf-api/rent-loan-multi-info/get-data.tsx
@@ -1,20 +1,20 @@
-'use server'
+"use server";
 
 export async function getLoanRateData(loanYm) {
-
   console.log("getLoanRateData() start...");
 
-  const apiStr = ""
-    + "?serviceKey=PW2VvwTvkcs%2FWMVLduXzeRL0BPjOYH%2B0wMnsQiyy5UgcrukEjAurATJUNkeA7T%2Bj47s3GAmLzHduip%2BfbxESlQ%3D%3D"
-    + "&pageNo=1"
-    + "&numOfRows=30"
-    + "&dataType=JSON"
-    ;
-
-  const res = await fetch("https://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list"
-    + apiStr
-    + "&loanYm=" + loanYm,
-    { next: { revalidate: 600 } }
+  const apiStr =
+    "" +
+    "?serviceKey=FMlDZea1lXraZUHfgkfivAF3QAUV%2FNkhRUFRRfHYn1osLEHq2oUYbWhqbrlZ67ltk9DOeIFdLnLSWRbHJxtt6g%3D%3D" +
+    "&pageNo=1" +
+    "&numOfRows=30" +
+    "&dataType=JSON";
+  const res = await fetch(
+    "https://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list" +
+      apiStr +
+      "&loanYm=" +
+      loanYm,
+    { next: { revalidate: 600 } },
   );
 
   const rentJson = await res.json();
